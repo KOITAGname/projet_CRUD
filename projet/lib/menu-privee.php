98c8a7c96e72d1223edd9cbcfaf1c374213d30c9
Gname K <gname.koita@yahoo.fr>
2023-02-14 15:24:20 +0100

    fin!!!!!!!!!!

--- projet/lib/menu-privee.php
+++ projet/lib/menu-privee.php
@@ -2,5 +2,4 @@
   <a href="http://localhost/php-initiation/projet/index.php?page=accueil&partie=privee" class="list-group-item ">Accueil back office</a>
   <a href="http://localhost/php-initiation/projet/index.php?page=page&partie=privee" class="list-group-item ">Gestion des pages</a>
   <a href="http://localhost/php-initiation/projet/index.php?page=user&partie=privee" class="list-group-item ">Gestion des profils utilisateurs</a>
-
 </div>
